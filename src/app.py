87f140386f785cb4e429647550429029e5292b2b
Alexander-sys18 <119635571+Alexander-sys18@users.noreply.github.com>
2023-08-15 01:07:59 +0000

    second commit

--- src/app.py
+++ src/app.py
@@ -1,12 +1,10 @@
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify
 from flask_migrate import Migrate
-from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, Planeta, Personaje, Favorito 
-
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -44,6 +42,53 @@ def obtener_personaje(personaje_id):
         return jsonify({"message": "Personaje no encontrado"}), 404
     return jsonify(personaje.serialize()), 200
 
+@app.route('/planets', methods=['GET'])
+def listar_planetas():
+    planetas = Planeta.query.all()
+    planetas_serializados = [p.serialize() for p in planetas]
+    return jsonify(planetas_serializados), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def obtener_planeta(planet_id):
+    planeta = Planeta.query.get(planet_id)
+    if planeta is None:
+        return jsonify({"message": "Planeta no encontrado"}), 404
+    return jsonify(planeta.serialize()), 200
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users_serialized = [{"id": user.id, "username": user.username} for user in users]
+    return jsonify(users_serialized)
+
+@app.route('/users/favorites', methods=['GET'])
+def get_user_favorites():
+    
+    return jsonify({"favorites": []})  # Debes implementar esta lógica
+
+# @app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+# def add_favorite_planet(planet_id):
+#     
+#     return jsonify({"message": "Favorite planet added"})
+
+# @app.route('/favorite/people/<int:people_id>', methods=['POST'])
+# def add_favorite_people(people_id):
+#  
+#     return jsonify({"message": "Favorite people added"})
+
+# @app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+# def remove_favorite_planet(planet_id):
+#     
+#     return jsonify({"message": "Favorite planet removed"})
+
+# @app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+# def remove_favorite_people(people_id):
+#   
+#     return jsonify({"message": "Favorite people removed"})
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -3,20 +3,51 @@
 # db = SQLAlchemy()
 
 # class User(db.Model):
+#     __tablename__ = 'usuario'
 #     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     password = db.Column(db.String(80), unique=False, nullable=False)
-#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+#     email = db.Column(db.String(250), nullable=False, unique=True)
+#     username = db.Column(db.String(250), nullable=False, unique=True)
+#     password = db.Column(db.String(250), nullable=False)
+#     subscription_date = db.Column(db.String(250))
 
-#     def __repr__(self):
-#         return '<User %r>' % self.username
 
+# class Planeta(db.Model):
+#     __tablename__ = 'planeta'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(250), nullable=False)
+#     description = db.Column(db.String(250))
 #     def serialize(self):
 #         return {
 #             "id": self.id,
-#             "email": self.email,
-#             # do not serialize the password, its a security breach
+#             "name": self.name,
+#             "description": self.description
 #         }
+
+# class Personaje(db.Model):
+#     __tablename__ = 'personaje'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(250), nullable=False)
+#     description = db.Column(db.String(500))
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "name": self.name,
+#             "description": self.description
+#         }
+
+# class Favorito(db.Model):
+#     __tablename__ = 'favorito'
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     planet_id = db.Column(db.Integer, db.ForeignKey('planeta.id'), nullable=True)
+#     people_id = db.Column(db.Integer, db.ForeignKey('personaje.id'), nullable=True)
+
+#     usuario = db.relationship(User, back_populates='favoritos')
+#     planeta = db.relationship(Planeta)
+#     personaje = db.relationship(Personaje)
+    
+
+# User.favoritos = db.relationship(Favorito, back_populates='usuario')
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -28,6 +59,7 @@ class User(db.Model):
     username = db.Column(db.String(250), nullable=False, unique=True)
     password = db.Column(db.String(250), nullable=False)
     subscription_date = db.Column(db.String(250))
+    favorites = db.relationship('Favorito', back_populates='user', cascade='all, delete-orphan')
 
 class Planeta(db.Model):
     __tablename__ = 'planeta'
@@ -35,21 +67,33 @@ class Planeta(db.Model):
     name = db.Column(db.String(250), nullable=False)
     description = db.Column(db.String(250))
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+
 class Personaje(db.Model):
     __tablename__ = 'personaje'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
     description = db.Column(db.String(500))
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+
 class Favorito(db.Model):
     __tablename__ = 'favorito'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
-    planet_id = db.Column(db.Integer, db.ForeignKey('planeta.id'))
-    character_id = db.Column(db.Integer, db.ForeignKey('personaje.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planeta.id'), nullable=True)
+    people_id = db.Column(db.Integer, db.ForeignKey('personaje.id'), nullable=True)
 
-    usuario = db.relationship(User, back_populates='favoritos')
+    user = db.relationship(User, back_populates='favorites')
     planeta = db.relationship(Planeta)
     personaje = db.relationship(Personaje)
-
-User.favoritos = db.relationship(Favorito, back_populates='usuario')
